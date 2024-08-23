e864c0af4546c8d1491209f32169994aad6adf60
im-hamza-dev <hi.hamza.dev@gmail.com>
2024-08-23 12:29:20 +0500

    added cors allowed all origins

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const port = process.env.PORT;
 // Configure Multer for file upload
 const upload = multer({ dest: "uploads/" });
 app.use(cors());
+app.options("*", cors())
 
 // Endpoint to handle PDF upload and send it via email
 app.post("/send-pdf", upload.single("file"), (req, res) => {
